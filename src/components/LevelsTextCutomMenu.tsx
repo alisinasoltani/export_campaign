a0e0f568fc5d9484fb4f23b7424a03038809c30a
Alisina <alisinasoltani82@gmail.com>
2025-02-28 05:01:24 +0330

    fix: set text custom menu floating action to left

--- src/components/LevelsTextCutomMenu.tsx
+++ src/components/LevelsTextCutomMenu.tsx
@@ -66,26 +66,26 @@ const LevelsTextCutomMenu = ({ selectedStyle, background_colors_options, font_fa
         </div>
     </div>
     ) : (
-    <Fab component="div" className="fixed right-[3rem] bottom-5 shadow-lg" size="small" color="secondary" aria-label="add">
+    <Fab component="div" className="fixed left-[2.2rem] bottom-5 shadow-lg" size="small" color="secondary" aria-label="add">
         <DropdownMenu>
             <DropdownMenuTrigger className="border-none outline-none">
                 <Image src={custom_icon} width={24} alt="click to cutomize" />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="flex flex-col items-end bg-transparent border-none absolute shadow-none" style={{ bottom: "2rem", right: "-1.7rem" }}>
+            <DropdownMenuContent className="flex flex-col items-start bg-transparent border-none absolute shadow-none" style={{ bottom: "2rem", left: "-1.7rem" }}>
                 <DropdownMenuItem onClick={(event) => handleChangeBackgroundColor(event, ((selectedStyle.background_color_index+1) % background_colors_options.length))}>
-                    <div className='flex flex-row-reverse min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
+                    <div className='flex flex-row min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
                         <div className='flex justify-center items-center w-8 h-8 rounded-full border-2 border-[#CF3828] shadow-md' style={{ backgroundColor: background_colors_options[selectedStyle.background_color_index].color, color: background_colors_options[selectedStyle.background_color_index].type == "dark" ? "#F5F5F5" : "#000000" }}>آ</div>
                         <h5 className="bg-white shadow-md rounded-2xl px-4 py-1.5">رنگ پس زمینه</h5>
                     </div>
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={(event) => handleFontChange(event, ((selectedStyle.font_index+1) % font_families.length))}>
-                    <div className='flex flex-row-reverse min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
+                    <div className='flex flex-row min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
                         <h5 className={`bg-white flex justify-center items-center w-8 h-8 border-2 border-[#CF3828] shadow-md rounded-full font-bold ${font_families[selectedStyle.font_index].css_class}`}>آ</h5>
                         <h5 className="bg-white shadow-md rounded-2xl px-4 py-1.5">{font_families[selectedStyle.font_index].name}</h5>
                     </div>
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={(event) => handleFontSizeChange(event, (selectedStyle.font_size+6))}>
-                    <div className='flex flex-row-reverse min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
+                    <div className='flex flex-row min-w-max gap-2 justify-start items-center text-sm border-none outline-none'>
                         <h5  className="bg-white flex justify-center items-center w-8 h-8 border-2 border-[#CF3828] shadow-md rounded-full font-bold">{selectedStyle.font_size}</h5>
                         <h5 className="bg-white shadow-md rounded-2xl px-4 py-1.5">اندازه</h5>
                     </div>
